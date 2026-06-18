80ae464c9e0b0b4b3c868303d9732f10a9cc6a8d
JJJ320 <gabrizao122@gmail.com>
2026-06-18 17:09:24 -0300

    Configurar Firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsrzFZBuVDIgHoX_uvQO11PKMFKz8IEnI",
+  authDomain: "portal-ael.firebaseapp.com",
+  projectId: "portal-ael",
+  storageBucket: "portal-ael.firebasestorage.app",
+  messagingSenderId: "940388244025",
+  appId: "1:940388244025:web:39fc8bae8dc129deac8be8",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
