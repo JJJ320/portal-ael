ca3b6270ecbdd35397c6ca7ad027c02d4480148d
JJJ320 <gabrizao122@gmail.com>
2026-06-19 17:37:05 -0300

    Implementar sistema de rotas protegidas completo

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "../pages/Login";
 import Verificacao from "../pages/Verificacao";
+import Home from "../pages/Home";
+
 import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* rota padrão */}
         <Route path="/" element={<Navigate to="/login" />} />
 
+        {/* login público */}
         <Route path="/login" element={<Login />} />
 
+        {/* verificação (precisa estar logado) */}
         <Route
           path="/verificacao"
           element={
@@ -19,6 +26,20 @@ export default function AppRoutes() {
             </ProtectedRoute>
           }
         />
+
+        {/* home (precisa login + cargo válido) */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute allowedRoles={["aluno", "lider", "funcionario"]}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* fallback (qualquer rota inválida volta pro login) */}
+        <Route path="*" element={<Navigate to="/login" />} />
+
       </Routes>
     </BrowserRouter>
   );
